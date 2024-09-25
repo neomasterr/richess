771e2fc3ab0503ad83e16c7df8ef27e0c791c538
neomasterr <neomasterr@mail.ru>
2024-09-25 20:50:23 +0400

    knight moves

--- engine/pieces/knight.js
+++ engine/pieces/knight.js
@@ -12,6 +12,35 @@ Object.defineProperty(Knight.prototype, 'constructor', {
     enumerable: false,
 });
 
+Knight.prototype.moves = function () {
+    return [
+        {direction: 'up', branches: ['left', 'right']},
+        {direction: 'right', branches: ['up', 'down']},
+        {direction: 'down', branches: ['left', 'right']},
+        {direction: 'left', branches: ['down', 'up']},
+    ].reduce((moves, pattern) => {
+        const forwardCell = this.cell.neighbour(pattern.direction);
+        if (!forwardCell) {
+            return moves;
+        }
+
+        const nextForwardCell = forwardCell.neighbour(pattern.direction);
+        if (!nextForwardCell) {
+            return moves;
+        }
+
+        pattern.branches.forEach(direction => {
+            const branchCell = nextForwardCell.neighbour(direction);
+
+            if (branchCell && (branchCell.empty() || (branchCell.piece && branchCell.piece.color != this.color))) {
+                moves.push(branchCell);
+            }
+        });
+
+        return moves;
+    }, []);
+}
+
 Knight.make = function (color, x, y, options = {}) {
     return new Knight(color, x, y, options);
 }
